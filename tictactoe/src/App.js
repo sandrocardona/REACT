d23fec0b566fd66abffac6642bbffb81fe09c8bb
sandrocardona <sandrocardona41@gmail.com>
2023-12-13 20:44:21 +0100

    13 dic

---
+++ tictactoe/src/App.js
@@ -0,0 +1,30 @@
+function Square({value}){
+  function handleClick(){
+    
+  }
+  return <button className="square">{value}</button>;
+
+}
+
+export default function Board() {
+  return ( 
+    <>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+      <div className="board-row">
+        <Square/>
+        <Square/>
+        <Square/>
+      </div>
+    </>
+    );
+}
+
